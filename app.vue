ef5b3c2bcaf23a996c3d8d309b2cf7b135ead123
themari0pl <156568892+themari0pl@users.noreply.github.com>
2024-03-02 22:42:21 +0100

    Init nuxt & packages (#1)
    * Init nuxt & packages
    
    * Updating Init per reviewer request
    
    * Changes to code post CR
    
    ---------
    
    Co-authored-by: themari0pl <barwac18@gmail.com>

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <SideBar/>
+    <NuxtPage/>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  alias: {"@": resolve(__dirname, '/')},
+  css: ["~/assets/main.scss"]
+})
