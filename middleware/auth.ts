46009f2baf1eb384139b97adc42c1b6ae3a3bb05
lobarzanek <lukiob@gmail.com>
2024-03-06 16:55:49 +0100

    Adjust variable to use keys.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,15 +1,13 @@
 const isAuthenticated = async (): Promise<boolean> => {
-  const sessionToken = sessionStorage.getItem("AUTH_KEY");
+  const sessionToken = sessionStorage.getItem(SESSION_STORAGE_AUTH_KEY);
   if (sessionToken == null) {
     return false;
   }
   const response = await $fetch("/api/login", {
     method: "GET",
   });
-  if (response.token === sessionToken) {
-    return true;
-  }
-  return false;
+
+  return response.token === sessionToken;
 };
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
