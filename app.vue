6e290a40174e2fbf55d7108e950c417fee1077cd
Adrian <adrian.suwala11@interia.pl>
2024-04-09 21:51:01 +0200

    Integrate header with sidebar

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="flex">
-    <SideBar />
+  <div>
     <NuxtPage />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,27 @@
 <script setup>
 import SummariesHeader from '../components/Header/SummariesHeader.vue'
+import Sidebar from '../components/Side/Bar.vue'
 </script>
+
 <template>
-  <SummariesHeader/>
-  <h3><span>Index</span></h3>
-  <button class="btn">Button</button>
+  <div class="layout">
+    <Sidebar class="layout-sidebar"/>
+    <div class="layout-content">
+      <SummariesHeader/>
+      <div class="p-4">
+        <!-- Content goes here -->
+      </div>    
+    </div>
+  </div>
 </template>
+
 <style scoped lang="scss">
-h3 {
-  span {
-    color: green;
-  }
+.layout {
+  display: flex;
+  min-height: 100vh;
+}
+
+.layout-content {
+  flex-grow: 1;
 }
 </style>
