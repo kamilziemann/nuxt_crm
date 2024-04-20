0382517f47b60d5169a89c6b5b289bfed7eb7533
markcir <irecomv@gmail.com>
2024-04-20 19:46:26 +0200

    Add ProductsTable-components

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <script setup>
-import SummariesHeader from '../components/Header/SummariesHeader.vue'
+import SummariesHeader from '../components/Header/SummariesHeader.vue';
+//
+import ProductTable from '~/components/ProductsTable/ProductsTable.vue';
 </script>
 
 <template>
-  <div class="flex justify-center mt-10 w-full">
+  <div class="flex justify-center mt-10 mb-10 w-full">
     <div>
       <SummariesHeader />
+      <ProductsTable />
     </div>
   </div>
 </template>
