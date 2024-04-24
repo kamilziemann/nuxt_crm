ffcd45e6570bd3f5391e47aaab63ec77a2455f46
Kamil Ziemann <kamilziemann1@gmail.com>
2024-04-24 22:23:23 +0200

    feature: Added table,modal and example page with nested page with id

---
+++ pages/example/[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute();
+
+console.log(route.params.id);
+</script>
+
+<template>
+	<div class="flex justify-center mt-10 w-full">
+		<div>Id of employee: {{ route.params.id }}</div>
+	</div>
+</template>
