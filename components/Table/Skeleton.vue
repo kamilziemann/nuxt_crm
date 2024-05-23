cd2671ac97bfac58e485790c3343a2cc774c0632
awalczak380 <awalczak380@gmail.com>
2024-05-23 20:51:33 +0200

    Add skeleton table

---
+++ components/Table/Skeleton.vue
@@ -0,0 +1,53 @@
+<script setup>
+const props = defineProps({
+  headers: {
+    type: Array,
+    default: () => [],
+  },
+  rowsNumber: {
+    type: Number,
+    default: () => 3,
+  },
+  title: {
+    type: String,
+    default: () => "",
+  },
+  buttonLabel: {
+    type: String,
+    default: () => "",
+  },
+  showButton: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
+
+<template>
+  <div class="w-full">
+    <div class="flex justify-between items-center mb-4 w-full">
+      <h2>{{ title }}</h2>
+      <div v-if="showButton" class="btn skeleton :hover-none text-opacity-0">
+        {{ buttonLabel }}
+      </div>
+    </div>
+    <div class="overflow-x-auto">
+      <table class="table">
+        <thead>
+          <tr>
+            <th v-for="(header, index) in headers" :key="index">
+              {{ header }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(, rowIndex) in rowsNumber" :key="rowIndex">
+            <td v-for="(, cellIndex) in headers" :key="cellIndex">
+              <div class="skeleton w-2/3 h-6"></div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -1,46 +1,59 @@
 <script setup>
 const showModal = ref(false);
+const tableData = ref([]);
+const loading = ref(false);
 
 const fields = [
-	{ label: "E-mail", model: "e-mail" },
-	{ label: "Price", model: "price" },
+  { label: "E-mail", model: "e-mail" },
+  { label: "Price", model: "price" },
   { label: "Quantity", model: "Quantity" },
-  
 ];
 const submitModalForm = (formData) => {
-	console.log("Form Data:", formData);
-	console.log("Name:", formData.name);
-	showModal.value = false;
+  console.log("Form Data:", formData);
+  console.log("Name:", formData.name);
+  showModal.value = false;
 };
 const handleOpen = () => {
-	showModal.value = true;
+  showModal.value = true;
 };
 const handleClose = () => {
-	showModal.value = false;
+  showModal.value = false;
 };
 
+const fetchData = async () => {
+  loading.value = true;
+
+  //timespan to simulate waiting for api response
+  await new Promise((resolve) => setTimeout(resolve, 2500));
+  loading.value = false;
+};
+
+onMounted(() => fetchData());
 </script>
 
 <template>
-    <div class="card-normal w-full max-w-[800px] mx-auto mt-10">
-        <Table
-            :headers="['ID', 'E-mail','Price', 'Quantity']"
-            :rows="[
-                [1, 'CyGanderton@gmail.com', '30.00€', '2'],
-                [2, 'HartHagerty@gmail.com', '35.00€', '1'],
-                [3, 'BriceSwyre@gmail.com', '26.90€', '3'],
-            ]"
-            title="Order details"
-            buttonLabel="Add new order"
-            :onButtonClick="handleOpen"
-            @row-click="handleRowClick" 
-        />
-        <Modal
-            :isOpen="showModal"
-            :fields="fields"
-            :onSubmit="submitModalForm"
-            :onClose="handleClose"
-            :title="`Add new order`"
-        />
-    </div>
+  <div class="card-normal w-full max-w-[800px] mx-auto mt-10">
+    <TableSkeleton
+      v-if="loading"
+      :headers="['ID', 'E-mail', 'Price', 'Quantity']"
+      :rowsNumber="3"
+      title="Orders"
+      buttonLabel="Add new order"
+    />
+    <Table
+      v-else
+      :headers="['ID', 'E-mail', 'Price', 'Quantity']"
+      :rows="tableData"
+      title="Orders"
+      buttonLabel="Add new order"
+      :onButtonClick="handleOpen"
+    />
+    <Modal
+      :isOpen="showModal"
+      :fields="fields"
+      :onSubmit="submitModalForm"
+      :onClose="handleClose"
+      :title="`Add new order`"
+    />
+  </div>
 </template>
