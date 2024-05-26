c1fc1b98e7505d0c2f65210693e1877618b03839
lobarzanek <lukiob@gmail.com>
2024-05-26 13:32:10 +0200

    Add logout button

--- components/Side/Bar.vue
+++ components/Side/Bar.vue
@@ -1,28 +1,18 @@
-<script>
-import FontSizer from "./FontSizer.vue";
-import ToggleMode from "./ToggleMode.vue";
+<script setup>
 import DashboardIcon from "../../assets/svg/DashboardIcon.vue";
 import ProductIcon from "../../assets/svg/ProductIcon.vue";
 import CustomersIcon from "../../assets/svg/CustomersIcon.vue";
 import TruckIcon from "../assets/svg/TruckIcon.vue";
+import { SESSION_STORAGE_AUTH_KEY } from "~/utils/keys.ts";
 
-export default {
-  components: {
-    FontSizer,
-    DashboardIcon,
-    ProductIcon,
-    CustomersIcon,
-    ToggleMode,
-    TruckIcon,
-  },
-  computed: {
-    $style() {
-      return {
-        link: "btn flex w-[250px] h-[50px] items-center rounded-[8px] mt-[10px] bg-base-100 justify-start",
-        text: "ml-[14px]",
-      };
-    },
-  },
+const style = {
+  link: "btn flex w-[250px] h-[50px] items-center rounded-[8px] mt-[10px] bg-base-100 justify-start",
+  text: "ml-[14px]",
+};
+
+const handleLogout = async () => {
+  sessionStorage.setItem(SESSION_STORAGE_AUTH_KEY, "");
+  await navigateTo("/login");
 };
 </script>
 <template>
@@ -33,34 +23,39 @@ export default {
       <h1 class="text-[26px] p-[20px]">Dashboard</h1>
       <div class="mt-[30px]">
         <NuxtLink to="/">
-          <div :class="$style.link" class="group duration-500">
+          <div :class="style.link" class="group duration-500">
             <DashboardIcon />
-            <p :class="$style.text">Dashboard</p>
+            <p :class="style.text">Dashboard</p>
           </div>
         </NuxtLink>
         <NuxtLink to="/products">
-          <div :class="$style.link" class="group duration-500">
+          <div :class="style.link" class="group duration-500">
             <ProductIcon />
-            <p :class="$style.text">Products</p>
+            <p :class="style.text">Products</p>
           </div>
         </NuxtLink>
         <NuxtLink to="/customers">
-          <div :class="$style.link" class="group duration-500">
+          <div :class="style.link" class="group duration-500">
             <CustomersIcon />
-            <p :class="$style.text">Customers</p>
+            <p :class="style.text">Customers</p>
           </div>
         </NuxtLink>
         <NuxtLink to="/orders">
-          <div :class="$style.link" class="group duration-500">
+          <div :class="style.link" class="group duration-500">
             <TruckIcon />
-            <p :class="$style.text">Orders</p>
+            <p :class="style.text">Orders</p>
           </div>
         </NuxtLink>
       </div>
     </div>
-    <div class="flex items-center w-full justify-evenly mb-5">
-      <ToggleMode />
-      <FontSizer />
+    <div class="flex flex-col items-center gap-5 w-full">
+      <button @click="handleLogout" class="btn btn-outline w-5/6">
+        Logout
+      </button>
+      <div class="flex items-center w-full justify-evenly mb-5">
+        <SideToggleMode />
+        <SideFontSizer />
+      </div>
     </div>
   </div>
 </template>
